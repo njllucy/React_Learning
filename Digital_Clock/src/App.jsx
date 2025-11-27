07e5865c2cc0437ad0d4fd18af8445be94f65df3
njllucy <njllucy1221@gmail.com>
2025-11-27 18:28:30 +0600

    added files of digital_clock

---
+++ Digital_Clock/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import DigitalClock from './DigitalClock'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <DigitalClock />
+    </>
+  )
+}
+
+export default App
